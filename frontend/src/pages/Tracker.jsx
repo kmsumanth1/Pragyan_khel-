4bafac4ff2a9196314edae87d43e9255ae5795f0
Fiona-ali <fionaali2006@gmail.com>
2026-02-21 00:20:01 +0530

    tracker

---
+++ frontend/src/pages/Tracker.jsx
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import { useRef } from "react";
+import VideoCanvas from "../components/VideoCanvas";
+import ControlPanel from "../components/ControlPanel";
+import FPSCounter from "../components/FPSCounter";
+
+export default function Tracker() {
+  const { source } = useParams();
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  return (
+    <div className="tracker fade-in">
+      <div className="navbar">AI Dynamic Tracker</div>
+
+      <ControlPanel source={source} videoRef={videoRef} />
+      <VideoCanvas videoRef={videoRef} canvasRef={canvasRef} />
+      <FPSCounter />
+    </div>
+  );
+} 
